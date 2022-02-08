490db8738318233b92a12fdf0ef3f3166eae11ae
Sushant Patial <sushantpatial18@gmai.com>
2022-02-08 19:10:50 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,52 +1,9 @@
-$('body').bind('mousewheel', function() {
-  if ($('body').scrollTop() > 130) {
-    $(".navbar").addClass("nav-scroll");
-    $('.navbar-logo img').attr('src','images/logo.png');
+$(".add").on('click', function() {
+  if ($(this).html() == 'Added') {
+    $(this).html('Add to cart');
   } else {
-    $(".navbar").removeClass("nav-scroll");
-    $('.navbar-logo img').attr('src','images/logo-white.png');
+    $(this).html('Added');
   }
-});
-$('body').bind('touchmove', function() {
-  if ($('body').scrollTop() > 130) {
-    $(".navbar").addClass("nav-scroll");
-    $('.navbar-logo img').attr('src','images/logo.png');
-  } else {
-    $(".navbar").removeClass("nav-scroll");
-    $('.navbar-logo img').attr('src','images/logo-white.png');
-  }
-});
+})
 
 
-setInterval(function() {
-  if ($('body').scrollTop() > 130) {
-    $(".navbar").addClass("nav-scroll");
-    $('.navbar-logo img').attr('src','images/logo.png');
-  } else {
-    $(".navbar").removeClass("nav-scroll");
-    $('.navbar-logo img').attr('src','images/logo-white.png');
-  }
-}, 300)
-
-
-$('.navbar ul li:nth-child(1) a').on('click', function() {
-  $.smoothScroll({
-    scrollElement: $('body'),
-    scrollTarget: '#home'
-  });
-  return false;
-});
-$('.navbar ul li:nth-child(2) a').on('click', function() {
-  $.smoothScroll({
-    scrollElement: $('body'),
-    scrollTarget: '#about'
-  });
-  return false;
-});
-$('.navbar ul li:nth-child(3) a').on('click', function() {
-  $.smoothScroll({
-    scrollElement: $('body'),
-    scrollTarget: '#pricing'
-  });
-  return false;
-});

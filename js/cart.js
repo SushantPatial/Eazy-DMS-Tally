d52edd43ac6d92e3bde3b16d62b08a8973793a96
Sushant Patial <sushantpatial18@gmai.com>
2022-02-09 14:22:13 +0530

    cart.html added

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
   let countDisplayGold = 0;
   $(".plus-minus-gold .add-button").on('click', function() {
@@ -22,12 +22,15 @@ $(document).ready(function(){
     $(".plus-minus-silver .count-display").text(countDisplaySilver);
   }); 
 
-  $(".add-button").on('click', function() {
-    $(".cart-counter").text(countDisplayGold + countDisplaySilver);
-  }); 
-  $(".sub-button").on('click', function() {
-    $(".cart-counter").text(countDisplayGold + countDisplaySilver);
-  }); 
-});
+  let silverTotal = 0;
+  let goldTotal = 0;
+  $('.plus-minus').on('click', function() {
+    silverTotal = parseInt($(".plus-minus-silver .count-display").html()) * 11682;
+    $('.silver-total').html(silverTotal); 
 
-  
+    goldTotal = parseInt($(".plus-minus-gold .count-display").html()) * 35046 ;
+    $('.gold-total').html(goldTotal); 
+
+    $('.cart-total').html(silverTotal + goldTotal); 
+  })
+})

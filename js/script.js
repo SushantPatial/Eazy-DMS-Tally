e66b8199d0194911e339f701e9b415172945f883
Sushant Patial <sushantpatial18@gmai.com>
2022-02-09 18:52:05 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
 
   let countDisplayGold = 0;
-  $(".plus-minus-gold .add-button").on('click', function() {
+  $(".add-to-cart-gold .add-button").on('click', function() {
     countDisplayGold += +1;
-    $(".plus-minus-gold .count-display").text(countDisplayGold);
+    $(".add-to-cart-gold .count-display").text(countDisplayGold);
   }); 
-  $(".plus-minus-gold .sub-button").on('click', function() {
+  $(".add-to-cart-gold .sub-button").on('click', function() {
     countDisplayGold -= +1;
     countDisplayGold = countDisplayGold < 0 ? 0:countDisplayGold;
-    $(".plus-minus-gold .count-display").text(countDisplayGold);
+    $(".add-to-cart-gold .count-display").text(countDisplayGold);
   }); 
 
   let countDisplaySilver = 0;
-  $(".plus-minus-silver .add-button").on('click', function() {
+  $(".add-to-cart-silver .add-button").on('click', function() {
     countDisplaySilver += +1;
-    $(".plus-minus-silver .count-display").text(countDisplaySilver);
+    $(".add-to-cart-silver .count-display").text(countDisplaySilver);
   }); 
-  $(".plus-minus-silver .sub-button").on('click', function() {
+  $(".add-to-cart-silver .sub-button").on('click', function() {
     countDisplaySilver -= +1;
     countDisplaySilver = countDisplaySilver < 0 ? 0:countDisplaySilver;
-    $(".plus-minus-silver .count-display").text(countDisplaySilver);
+    $(".add-to-cart-silver .count-display").text(countDisplaySilver);
   }); 
 
-  $(".add-button").on('click', function() {
+  $(".add-to-cart-silver .add-to-cart-button").on('click', function() {
     $(".cart-counter").text(countDisplayGold + countDisplaySilver);
   }); 
-  $(".sub-button").on('click', function() {
+  $(".add-to-cart-gold .add-to-cart-button").on('click', function() {
     $(".cart-counter").text(countDisplayGold + countDisplaySilver);
   }); 
 });
